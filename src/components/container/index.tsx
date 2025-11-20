b88f46c2e8eaba16a5c350270e2a61418d0c21bc
David <seuemail@exemplo.com>
2025-11-20 18:18:53 -0300

    Feat: Criando componente container e pagina dashboard

---
+++ src/components/container/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+const Container = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className='w-full max-w-7xl mx-auto px-2'>Container</div>
+    )
+}
+
+export default Container
