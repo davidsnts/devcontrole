2632d53ee1ba531ae4449436775419cb7b35748e
David <seuemail@exemplo.com>
2025-11-21 17:20:44 -0300

    Feat: Realizando login

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,23 @@
+"use client"
+
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import React from 'react'
-import { FiLogOut, FiUser } from 'react-icons/fi'
+import { FiLoader, FiLock, FiLogOut, FiUser } from 'react-icons/fi'
+
 
 const Header = () => {
+    const { status, data } = useSession();
+
+    async function handleLogin() {
+        await signIn();
+    }
+
+    async function handleLogout() {
+        await signOut();
+    }
+
+
     return (
         <header className='w-full flex items-center px-2 py-4 bg-white h-20 shadow-sm'>
             <div className='w-full flex items-center justify-between max-w-7xl mx-auto'>
@@ -12,15 +27,25 @@ const Header = () => {
                     </h1>
                 </Link>
 
-                <div className='flex gap-4 items-baseline'>
-                    <Link href={'/dashboard'}>
-                        <FiUser size={26} color='#4b5563' />
-                    </Link>
+                {status === "loading" && (
+                    <button><FiLoader className='animate-spin' size={26} color="4b5563" /></button>
+                )}
 
-                    <button>
-                        <FiLogOut size={26} color='#4b5563' />
-                    </button>
-                </div>
+                {status === "unauthenticated" && (
+                    <button onClick={handleLogin}><FiLock size={26} color="4b5563" /></button>
+                )}
+
+                {status === "authenticated" && (
+                    <div className='flex gap-4 items-baseline'>
+                        <Link href={'/dashboard'}>
+                            <FiUser size={26} color='#4b5563' />
+                        </Link>
+
+                        <button onClick={handleLogout}>
+                            <FiLogOut size={26} color='#4b5563' />
+                        </button>
+                    </div>
+                )}
             </div>
         </header>
     )
