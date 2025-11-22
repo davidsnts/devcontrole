748a0065a3ba0863e09cf33153b58a450c7f5b73
David <seuemail@exemplo.com>
2025-11-22 15:15:56 -0300

    Feat: Criando página de clientes

---
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CardCustomer = () => {
+    return (
+        <article className='flex flex-col border-slate-200 bg-gray-100 border-2 p-2 rounded-lg gap-2 hover:scale-105 duration-300'>
+            <h2>
+                <a className='font-bold'>Nome:</a> Mercado Silva
+            </h2>
+            <p><a className='font-bold'>Email:</a> teste@teste.com</p>
+            <p><a className='font-bold'>Telefone:</a> 32 991563818</p>
+            <button className='bg-red-500 px-4 rounded text-white mt-2 self-start'> Deletar </button>
+        </article>
+    )
+}
+
+export default CardCustomer
---
+++ src/app/dashboard/customer/new/page.tsx
@@ -0,0 +1,12 @@
+import Container from '@/components/container'
+import React from 'react'
+
+const NewCustomer = () => {
+  return (
+    <Container>
+        Pagina cadastro cliente
+    </Container>
+)
+}
+
+export default NewCustomer
