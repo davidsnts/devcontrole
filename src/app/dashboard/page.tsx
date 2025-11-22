2d5e3298a116001de29f42bb7a7858dcc352f358
David <seuemail@exemplo.com>
2025-11-22 12:32:22 -0300

    Feat: Implementando proteção de rotas

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,17 @@
 import Container from "@/components/container";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Dashboard(){
-    return(
+export default async function Dashboard() {
+
+    const session = await getServerSession(authOptions)
+
+    if (!session || !session.user) {
+        redirect('/');
+    }
+
+    return (
         <Container>
             <h1>Pagina dashboard</h1>
         </Container>
